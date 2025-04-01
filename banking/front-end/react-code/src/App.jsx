511d5a531a93aa8d610692a7ffdad74bb29e7ae7
Yu Chang <yuzi@Yus-MacBook-Pro.local>
2025-04-01 16:02:51 +0100

    first update

--- banking/front-end/react-code/src/App.jsx
+++ banking/front-end/react-code/src/App.jsx
@@ -1,3 +1,4 @@
+
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/AdminLogin";
 
---
+++ banking/front-end/react-code/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import LoginButton from "../components/LoginButton";
+function Login() {
+  return (
+    <div>
+      <LoginButton />
+    </div>
+  );
+}
+
+export default Login;
