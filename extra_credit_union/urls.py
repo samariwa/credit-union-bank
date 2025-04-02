abbb9c31d8a72f73865682171b712c47297871bd
“samariwa” <samuelmariwa@gmail.com>
2025-04-02 15:49:37 +0100

    backend urls bug fix

--- extra_credit_union/urls.py
+++ extra_credit_union/urls.py
@@ -20,7 +20,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('banking.urls')),
+    path('api/', include('banking.back-end.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
